c49324814f135a72930d574bb26ce3e88684a56e
Denis Malko <d.malko@partida.io>
2024-06-14 11:14:16 +0300

    Update runcommand function

--- cmd/filesubst/main.go
+++ cmd/filesubst/main.go
@@ -5,13 +5,16 @@ import (
 	"flag"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
 func main() {
-	var filename string
-	flag.StringVar(&filename, "f", "", "file to read")    //map -f flag to filename variable
-	flag.StringVar(&filename, "file", "", "file to read") //map --file flag to filename variable
+	var filename, outputFilename string
+	flag.StringVar(&filename, "f", "", "file to read")                       //map -f flag to filename variable
+	flag.StringVar(&filename, "file", "", "file to read")                    //map --file flag to filename variable
+	flag.StringVar(&outputFilename, "o", "", "file to write output to")      //map -o flag to outputFilename variable
+	flag.StringVar(&outputFilename, "output", "", "file to write output to") //map --output flag to outputFilename variable
 
 	// Parse the command-line arguments
 	flag.Parse()
@@ -20,6 +23,11 @@ func main() {
 		log.Fatalf("Please provide a filename via -f or --file")
 	}
 
+	if outputFilename == "" {
+		log.Fatalf("Please provide an output filename via -o or --output")
+	}
+
+	log.Printf("Templating file %s to %s", filename, outputFilename)
 	// Read configuration file
 	file, err := os.Open(filename)
 	if err != nil {
@@ -32,12 +40,22 @@ func main() {
 	// Create a new Scanner for the file
 	scanner := bufio.NewScanner(file)
 
+	// Define regex pattern
+	pattern := "\\w+=.*"
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		log.Fatalf("Failed to Compile regexp %v", err)
+	}
+
 	// Iterate through every line in file and substitute values if necessary
 	for scanner.Scan() {
 		line := scanner.Text()
+		if !re.MatchString(line) {
+			continue
+		}
 		split := strings.SplitN(line, "=", 2) // Split the line into key-value pair
 		if len(split) < 2 {
-			log.Fatalf("Invalid line: %s", line)
+			log.Fatalf("Invalid line %s", line)
 		}
 		key, value := split[0], split[1]
 
@@ -51,15 +69,15 @@ func main() {
 
 	// Check for Scanner errors
 	if err := scanner.Err(); err != nil {
-		log.Fatalf("Scanner error: %v", err)
+		log.Fatalf("Scanner error %v", err)
 	}
 
-	// Write back to file
+	// Write to the output file
 	newContent := strings.Join(lines, "\n")
-	err = os.WriteFile(filename, []byte(newContent), 0)
+	err = os.WriteFile(outputFilename, []byte(newContent), os.ModePerm)
 	if err != nil {
-		log.Fatalf("Failed to write to file: %v", err)
+		log.Fatalf("Failed to write to file %v", err)
 	}
 
-	log.Println("Configuration file updated successfully.")
+	log.Printf("Configuration file updated and written to %s successfully.", outputFilename)
 }
